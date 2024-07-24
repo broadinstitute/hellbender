c3669953b99059ae2f801b636ce7fb862f949aef
James <emeryj@broadinstitute.org>
2024-07-24 10:23:37 -0400

    restart? hopefully transient

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/HaplotypeCallerIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/haplotypecaller/HaplotypeCallerIntegrationTest.java
@@ -2157,7 +2157,7 @@ public class HaplotypeCallerIntegrationTest extends CommandLineProgramTest {
         }
     }
 
-    @Test(dataProvider="HaplotypeCallerTestInputs", groups={"bucket"})
+    @Test(dataProvider="HaplotypeCallerTestInputs", groups={"bucket"}) 
     public void testPileupCallingDRAGEN378OptimizedModeConsistentWithPastResults(final String inputFileName, final String referenceFileName) throws Exception {
         Utils.resetRandomGenerator();
 
